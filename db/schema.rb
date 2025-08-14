a76f525b668805260577374f023160cd9e63335f
Atefe Saeedi <atefe.saeedi1994@gmail.com>
2025-08-14 16:09:37 +0200

    rails new

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_14_110908) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_14_135823) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
+  enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
     t.text "comment"
